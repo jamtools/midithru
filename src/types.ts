ba713e6f3f695829c206c3a92c11a0f89b890df7
Michael Kochell <6913320+mickmister@users.noreply.github.com>
2024-04-20 14:06:57 -0400

    add biome and linting rules

--- src/types.ts
+++ src/types.ts
@@ -3,41 +3,52 @@ import {IMIDIInput, IMIDIOutput} from '@midival/core';
 type InputMidiDeviceConnectedEvent = {
     type: 'input_connected';
     device: IMIDIInput;
-}
+};
 
 type InputMidiDeviceDisconnectedEvent = {
     type: 'input_disconnected';
     device: IMIDIInput;
-}
+};
 
 export const MidiEventMatchers = {
-    isInputConnectedEvent: (event: MidiDeviceChangeEvent): event is InputMidiDeviceConnectedEvent => {
+    isInputConnectedEvent: (
+        event: MidiDeviceChangeEvent,
+    ): event is InputMidiDeviceConnectedEvent => {
         return event.type === 'input_connected';
     },
-    isInputDisconnectedEvent: (event: MidiDeviceChangeEvent): event is InputMidiDeviceDisconnectedEvent => {
+    isInputDisconnectedEvent: (
+        event: MidiDeviceChangeEvent,
+    ): event is InputMidiDeviceDisconnectedEvent => {
         return event.type === 'input_disconnected';
     },
-    isOutputConnectedEvent: (event: MidiDeviceChangeEvent): event is OutputMidiDeviceConnectedEvent => {
+    isOutputConnectedEvent: (
+        event: MidiDeviceChangeEvent,
+    ): event is OutputMidiDeviceConnectedEvent => {
         return event.type === 'output_connected';
     },
-    isOutputDisconnectedEvent: (event: MidiDeviceChangeEvent): event is OutputMidiDeviceDisconnectedEvent => {
+    isOutputDisconnectedEvent: (
+        event: MidiDeviceChangeEvent,
+    ): event is OutputMidiDeviceDisconnectedEvent => {
         return event.type === 'output_disconnected';
     },
-}
+};
 
-export const isSameMidiDevice = (a: IMIDIInput | IMIDIOutput, b: IMIDIInput | IMIDIOutput): boolean => {
+export const isSameMidiDevice = (
+    a: IMIDIInput | IMIDIOutput,
+    b: IMIDIInput | IMIDIOutput,
+): boolean => {
     return a.name === b.name && a.manufacturer === b.manufacturer;
-}
+};
 
 type OutputMidiDeviceConnectedEvent = {
     type: 'output_connected';
     device: IMIDIOutput;
-}
+};
 
 type OutputMidiDeviceDisconnectedEvent = {
     type: 'output_disconnected';
     device: IMIDIOutput;
-}
+};
 
 export type MidiDeviceChangeEvent =
     | InputMidiDeviceConnectedEvent
@@ -48,30 +59,34 @@ export type MidiDeviceChangeEvent =
 export type CurrentMidiDeviceState = {
     connectedInputDevices: IMIDIInput[];
     connectedOutputDevices: IMIDIOutput[];
-}
+};
 
 export type MidiDeviceChangeMessage = {
     state: CurrentMidiDeviceState;
     events?: MidiDeviceChangeEvent[];
-}
+};
 
 export const getTextForEvents = (events: MidiDeviceChangeEvent[]): string => {
     let newConnectedDevices: string[] = [];
     let newDisconnectedDevices: string[] = [];
     for (const event of events) {
         switch (true) {
-            case MidiEventMatchers.isInputConnectedEvent(event):
+            case MidiEventMatchers.isInputConnectedEvent(event): {
                 newConnectedDevices.push(event.device.name);
                 break;
-            case MidiEventMatchers.isInputDisconnectedEvent(event):
+            }
+            case MidiEventMatchers.isInputDisconnectedEvent(event): {
                 newDisconnectedDevices.push(event.device.name);
                 break;
-            case MidiEventMatchers.isOutputConnectedEvent(event):
+            }
+            case MidiEventMatchers.isOutputConnectedEvent(event): {
                 newConnectedDevices.push(event.device.name);
                 break;
-            case MidiEventMatchers.isOutputDisconnectedEvent(event):
+            }
+            case MidiEventMatchers.isOutputDisconnectedEvent(event): {
                 newDisconnectedDevices.push(event.device.name);
                 break;
+            }
         }
     }
 
@@ -80,14 +95,14 @@ export const getTextForEvents = (events: MidiDeviceChangeEvent[]): string => {
     newDisconnectedDevices = Array.from(new Set(newDisconnectedDevices));
 
     if (newConnectedDevices.length) {
-        text += `Connected: ${newConnectedDevices.join(', ')}`;
+        text += `Connected:\n${newConnectedDevices.join('\n')}`;
     }
 
     if (newDisconnectedDevices.length) {
         if (text.length) {
             text += '\n';
         }
-        text += `Disconnected: ${newDisconnectedDevices.join(', ')}`;
+        text += `Disconnected:\n${newDisconnectedDevices.join('\n')}`;
     }
 
     if (!text.length) {
@@ -95,4 +110,4 @@ export const getTextForEvents = (events: MidiDeviceChangeEvent[]): string => {
     }
 
     return text;
-}
+};
